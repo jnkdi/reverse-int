5a0462d07672dcee76ce5fa804b1148b72bb15b6
jnkdi <anaidik11@gmail.com>
2021-03-14 17:28:01 +0200

    add: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    n = String(Math.abs(n)).split('');
+    n.reverse();
+
+    while(n[0] == 0){
+        n.shift()
+    }
+    
+   return Number(n.join(''));
 }
